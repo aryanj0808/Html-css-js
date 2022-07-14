21545aa9da8ffed0fd5ccf639cb74cb9f83c1930
aryan Jadhav <aryanj0808@gmail.com>
2022-07-15 03:14:54 +0530

    01 Console Terms

---
+++ JavaScript/01_index.js
@@ -0,0 +1,9 @@
+console.time("Time required");
+console.log(5 + 5);
+alert("Error occur");
+console.error("we cannot run program");
+console.table({ aryan: "jadhav", jadhav: 35 });
+console.warn("This is prohibited");
+// console.clear();
+console.timeEnd("Time required");
+console.assert(100 < 76, "Age>76 is not possible");
