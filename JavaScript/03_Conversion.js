b32902a6d80d4ced72b2cb170508185081a230c3
aryan Jadhav <aryanj0808@gmail.com>
2022-07-15 15:39:02 +0530

    Conversion and Coercion

--- JavaScript/03_Conversion.js
+++ JavaScript/03_Conversion.js
@@ -1,1 +1,13 @@
-console.log(7 + 9);
+let date = new Date();
+console.log(date, typeof date);
+
+console.warn("Changing Datatype");
+//Converting datatype to string;
+
+date = String(new Date());
+console.log(date, typeof date);
+
+//Type of Coercion
+let num1 = Number("23");
+let num2 = 34;
+console.log(num1 + num2);
