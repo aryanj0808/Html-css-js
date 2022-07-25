1a5b594cd6afb17b93f177b0fdbf29a77daead59
aryan Jadhav <aryanj0808@gmail.com>
2022-07-25 23:11:26 +0530

    Arrays and Objects

---
+++ JavaScript/05_Arrays.js
@@ -0,0 +1,12 @@
+const marks = [22, 25, 21, 32, 55];
+const mix = [22, "orange", [21, 32], 55];
+console.log(marks);
+console.log(mix);
+
+console.log(marks.indexOf(25));
+marks.push(67);
+marks.pop();
+
+marks.reverse(67);
+
+console.log(marks);
