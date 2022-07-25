0a57ffc23ed243934450ca0ed400a48137a9bd9a
aryan Jadhav <aryanj0808@gmail.com>
2022-07-25 22:55:33 +0530

    Properties and Methods of Strings

---
+++ JavaScript/04_Strings.js
@@ -0,0 +1,16 @@
+let str1 = "THis is string 1 ";
+
+let str2 = "This is string 2";
+console.log(str1 + " " + str2);
+
+str1 = str1.concat("JOint", " str2");
+
+console.log(str1);
+console.log(str1.length);
+console.log(str1.toLowerCase());
+console.log(str1.toUpperCase());
+console.log(str1[0]);
+console.log(str1.indexOf("string"));
+console.log(str1.lastIndexOf("string"));
+
+console.log(str1.replace("THis", "TO"));
