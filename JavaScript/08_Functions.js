b9093bba2e77bea27616869c5760875ba4eddf56
aryan Jadhav <aryanj0808@gmail.com>
2022-07-26 00:17:51 +0530

    Functions

---
+++ JavaScript/08_Functions.js
@@ -0,0 +1,19 @@
+let name = "Aryan";
+let name1 = "Harry";
+let name2 = "assax";
+// console.log(`Happy Birthday ${name} `);
+// console.log(`Happy Birthday ${name1} `);
+
+function greeting(name) {
+  console.log(`Happy Birthday ${name} `);
+}
+
+greeting(name);
+greeting(name1);
+greeting(name2);
+
+arr = ["fruits", "veges", "meals"];
+
+arr.forEach(function (element, index, array) {
+  console.log(element, index);
+});
