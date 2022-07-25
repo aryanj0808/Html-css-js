fb765dca5e9e8c954f114557f32c1cb5c951f487
aryan Jadhav <aryanj0808@gmail.com>
2022-07-25 23:42:13 +0530

    Loops

---
+++ JavaScript/07_Loops.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 3; i++) {
+  console.log(i);
+}
+let j = 11;
+while (j < 14) {
+  console.log(j);
+  j += 1;
+}
+
+let k = 21;
+do {
+  console.log(k);
+  k += 1;
+} while (k < 10);
